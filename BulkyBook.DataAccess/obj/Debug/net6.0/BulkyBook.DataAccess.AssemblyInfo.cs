59dd390f413b9bcd4adabbed3a2a20d9302fde68
TahaKamalSewedy <taha.kamal@sewedy.com.eg>
2025-11-14 19:07:03 +0200

    .

--- BulkyBook.DataAccess/obj/Debug/net6.0/BulkyBook.DataAccess.AssemblyInfo.cs
+++ BulkyBook.DataAccess/obj/Debug/net6.0/BulkyBook.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBook.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07eaeba7964ca9ca4b9d2f92d46d8bc79fd88ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c675108322691ee4827e8d4876a0ee966294341e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBook.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBook.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
